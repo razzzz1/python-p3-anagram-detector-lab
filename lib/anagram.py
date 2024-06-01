887266ce0d2e946f89d969350a010c878fc22fe5
razzzz1 <ryanmaiyo1@gmail.com>
2024-06-01 10:01:58 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, possible_anagrams):
+        sorted_possible_anagrams = [sorted(possible_anagram.lower()) for possible_anagram in possible_anagrams]
+
+        matches = [anagram for anagram in possible_anagrams if sorted_possible_anagrams[possible_anagrams.index(anagram)] == self.word]
+
+        return matches
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
